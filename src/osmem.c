69fe257e748763a265afa001100cf66931acde07
gheorghe.petrica <gheorghe.petrica@stud.acs.upb.ro>
2023-11-07 21:44:09 +0200

    23 points

--- src/osmem.c
+++ src/osmem.c
@@ -15,12 +15,38 @@
 
 void *global_base = NULL;
 int ok = 0; //if the heap preallocation was made
+void *mapped_blocks = NULL; //list for holding the mapped blocks
 
 struct block_meta *find_free_block(struct block_meta **last, size_t size) { //have to search for a free a block
-	struct block_meta *current = global_base;
+	struct block_meta *current;
+	if (size >= MMAP_THRESHOLD)
+		current = mapped_blocks;
+	if (size < MMAP_THRESHOLD)
+		current = global_base;
 	struct block_meta *best_fit = NULL;
 	struct block_meta *ultimate = NULL;
 	size_t best_fit_size = SIZE_MAX;
+
+	//how to line the free blocks
+	struct block_meta* ptr = NULL;
+	if (size < MMAP_THRESHOLD) {
+		while (current) {
+			if (current->status == STATUS_FREE) {
+				if (ptr == NULL) {
+					ptr = current;
+				} else {
+					ptr->size += current->size;
+					ptr->next = current->next;
+					if (current->next)
+						current->next->prev = ptr;
+				}
+				current = ptr->next;
+			} else {
+					current = current->next;
+				}
+		}
+		current = global_base;
+	}
 	while (current) {
 		if (current->status == STATUS_FREE && current->size >= size) {
 			if (current->size < best_fit_size) {
@@ -81,12 +107,12 @@ void *os_malloc(size_t size)
 	size_t block_size = ALIGN(size);
 	if (block_size < 0)
 		return NULL;
-	if (!global_base) {
+	if (!global_base && block_size < MMAP_THRESHOLD) {
 		block = request_space(NULL, block_size);
 		if (!block)
 			return NULL;
 		global_base = block;
-	} else { //bad implementation
+	} else if (size < MMAP_THRESHOLD) {
 		struct block_meta *last = global_base;
 		last = find_free_block(&last, block_size);
 		if (!last->next && last->status != STATUS_FREE) { //if the last block of memory was already freed it will brk again
@@ -99,30 +125,55 @@ void *os_malloc(size_t size)
 			// to hold a bigger block: call sbrk again
 			size_t new_size;
 			struct block_meta *new;
-			if (size > last->size) {
-				new_size = ALIGN(size - last->size);
+			if (block_size > last->size) {
+				new_size = ALIGN(block_size - last->size);
 				new = sbrk(new_size);
 				if (new == (void *) -1)
 					return NULL;
 				last->size += new_size;
 			}
-			if (size < last->size) {
-				size_t remaining_size = last->size - size;
-				// new = (struct block_meta*)((char *)last + last->size);
-				// new->size = remaining_size;
-				// new->status = STATUS_FREE;
-				// new->prev = last;
-				// new->next = last->next;
-				// if (last->next)
-				// 	last->next->prev = new;
-				// last->next = new;
-				// last->size = size;
+			if (block_size <= last->size) { //see here when to call sbrk if the remaining size is not enough
+				size_t remaining_size = last->size - block_size;
+				if (remaining_size <= ALIGN(META_SIZE) + ALIGN(sizeof(char))) {
+					last->status = STATUS_ALLOC;
+					block = last;
+					return (block + 1);
+				}
+				// HOW TO SPLIT THE BLOCK?
+				last->size = block_size + ALIGN(META_SIZE);
+
+				// Calculate the address of the new free block
+				struct block_meta *new_block = (struct block_meta *)((char *)(last + 1) + block_size);
+				
+				new_block->status = STATUS_FREE;
+				new_block->size = ALIGN(remaining_size) - ALIGN(META_SIZE);
+				new_block->prev = last;
+				new_block->next = last->next;
+
+				// Update the next block's prev pointer if it exists
+				if (last->next) {
+					last->next->prev = new_block;
+				}
+
+				last->next = new_block;
 			}
 			
 			last->status = STATUS_ALLOC;
 			block = last;
 		}
 	}
+	if (block_size >= MMAP_THRESHOLD) { //doesnt work because find_free_block searches in global_base
+			if (!mapped_blocks) {
+				block = request_space(NULL, block_size);
+				if (!block)
+					return NULL;
+				mapped_blocks = block;
+			} else {
+				struct block_meta *last = mapped_blocks;
+				last = find_free_block(&last, block_size);
+				block = request_space(last, block_size);
+			}
+		}
 	return (block + 1);
 }
 
@@ -135,41 +186,24 @@ void os_free(void *ptr) {
         return;
 
     struct block_meta *block_ptr = get_block_ptr(ptr);
+	if (!block_ptr)
+		return NULL;
 
     if (block_ptr->status == STATUS_ALLOC) {
-        // If it was allocated using brk(), use sbrk() to deallocate.
-		// but i got to verify if the block next to it is free or not to create a bigger block
-		// i got to iterate to the right and left to find free blocks to use
 		block_ptr->status = STATUS_FREE;
-		size_t totalSize = block_ptr->size;
-		struct block_meta *current = block_ptr->next;
-		while (current) {
-			if (current->status == STATUS_FREE) {
-				totalSize += current->size;
-				block_ptr->next = current->next;
-				current = current->next;
-			} else {
-				break;
-			}
-		}
-		current = block_ptr->prev;
-		while (current) {
-			if (current->status == STATUS_FREE) {
-				totalSize += current->size;
-				block_ptr->prev = current->prev;
-				current = current->prev;
-			} else {
-				break;
-			}
-		}
+		//block_ptr->size = 0;
 		
     } else if (block_ptr->status == STATUS_MAPPED) {
-        // If it was allocated using mmap(), use munmap() to deallocate.
+		if (block_ptr == mapped_blocks) {
+            mapped_blocks = block_ptr->next;
+        } else {
+            block_ptr->prev->next = block_ptr->next;
+            if (block_ptr->next) {
+                block_ptr->next->prev = block_ptr->prev;
+            }
+        }
         munmap(block_ptr, block_ptr->size + META_SIZE);
     }
-
-    // Set the block status to FREE.
-    //block_ptr->status = STATUS_FREE;
 }
 
 
