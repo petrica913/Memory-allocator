21fcbcf33d3b2c96bf3583563924659c25e83a8a
gheorghe.petrica <gheorghe.petrica@stud.acs.upb.ro>
2023-11-11 12:58:46 +0200

    calloc done

--- src/osmem.c
+++ src/osmem.c
@@ -16,12 +16,13 @@
 void *global_base = NULL;
 int ok = 0; //if the heap preallocation was made
 void *mapped_blocks = NULL; //list for holding the mapped blocks
+size_t dimension = NULL;
 
 struct block_meta *find_free_block(struct block_meta **last, size_t size) { //have to search for a free a block
 	struct block_meta *current;
-	if (size >= MMAP_THRESHOLD)
+	if (size >= dimension)
 		current = mapped_blocks;
-	if (size < MMAP_THRESHOLD)
+	if (size < dimension)
 		current = global_base;
 	struct block_meta *best_fit = NULL;
 	struct block_meta *ultimate = NULL;
@@ -48,22 +49,22 @@ struct block_meta *request_space (struct block_meta *last, size_t size) {
 		return NULL;
 	struct block_meta *block;
 	void *request;
-	if (size < MMAP_THRESHOLD && ok != 0) {
+	if (size + META_SIZE < dimension && ok != 0) {
 		block = sbrk(META_SIZE + size);
 		if (block == (void*) -1)
 			return NULL;
 		block->status = STATUS_ALLOC;
 	}
-	if (size < MMAP_THRESHOLD && ok == 0) {
+	if (size + META_SIZE < dimension && ok == 0) {
 		block = sbrk(MMAP_THRESHOLD);
 		ok++;
 		block->status = STATUS_ALLOC;
-		block->size = MMAP_THRESHOLD;
+		block->size = dimension;
 		block->next = NULL;
 		block->prev = last;
 		return block;
 	}
-	if (size >= MMAP_THRESHOLD) {
+	if (size + META_SIZE >= dimension) {
 		request = mmap(NULL, size + META_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 		block = (struct block_metadata*) request;
 		assert((void*)block == request);
@@ -83,6 +84,7 @@ struct block_meta *request_space (struct block_meta *last, size_t size) {
 
 void *os_malloc(size_t size)
 {
+	dimension = MMAP_THRESHOLD;
 	struct block_meta *block;
 	size_t block_size = ALIGN(size);
 	if (block_size < 0)
@@ -187,14 +189,14 @@ void os_free(void *ptr) {
 		block_ptr->status = STATUS_FREE;
 		if (block_ptr->prev && block_ptr->prev->status == STATUS_FREE) {
 			struct block_meta *aux_ptr = block_ptr->next;
-			block_ptr->prev->size += block_ptr->size;
+			block_ptr->prev->size += block_ptr->size + ALIGN(META_SIZE);
 			block_ptr->prev->next = block_ptr->next;
 			if (aux_ptr)
 				aux_ptr->prev = block_ptr->prev;
 		}
-		if (block_ptr->next && block_ptr->next->status == STATUS_FREE) {
+		if (block_ptr->next && block_ptr->next->status == STATUS_FREE) { //might be wrong
 			struct block_meta *aux_ptr = block_ptr->prev;
-			block_ptr->next->size += block_ptr->size;
+			block_ptr->next->size += block_ptr->size + + ALIGN(META_SIZE);
 			block_ptr->next->prev = block_ptr->prev;
 			if (aux_ptr)
 				aux_ptr->next = block_ptr->next;
@@ -204,7 +206,8 @@ void os_free(void *ptr) {
 		if (block_ptr == mapped_blocks) {
             mapped_blocks = block_ptr->next;
         } else {
-            block_ptr->prev->next = block_ptr->next;
+			if (block_ptr->prev)
+            	block_ptr->prev->next = block_ptr->next;
             if (block_ptr->next) {
                 block_ptr->next->prev = block_ptr->prev;
             }
@@ -216,8 +219,106 @@ void os_free(void *ptr) {
 
 void *os_calloc(size_t nmemb, size_t size)
 {
-	/* TODO: Implement os_calloc */
-	return NULL;
+	if (!nmemb || !size)
+		return NULL;
+	size = size * nmemb;
+	int page_size = getpagesize();
+	dimension = page_size;
+	struct block_meta *block;
+	size_t block_size = ALIGN(size);
+	if (block_size < 0)
+		return NULL;
+	if (!global_base && block_size < page_size) {
+		block = request_space(NULL, block_size);
+		if (!block)
+			return NULL;
+		global_base = block;
+	} else if (size < page_size) {
+		struct block_meta *last = global_base;
+		last = find_free_block(&last, block_size);
+		if (!last->next) {
+			if (last->status != STATUS_FREE) {
+				block = request_space(last, block_size);
+				if (!block)
+					return NULL;
+				memset(block + 1, 0, block->size);
+				return (block + 1);
+			}
+			if (last->status == STATUS_FREE) {
+				struct block_meta *new;
+				int dim = block_size - last->size;
+				if (dim > 0) {
+					struct block_meta *current = sbrk(0);
+					new = sbrk(ALIGN(dim));
+					last->size += dim;
+					last->status = STATUS_ALLOC;
+					memset(last + 1, 0, last->size);
+					return last + 1;
+				} 
+				if (dim == 0) {
+					last->status = STATUS_ALLOC;
+					memset(last + 1, 0, last->size);
+					return last + 1;
+				}
+				if (dim < 0) {
+					dim = (-1) * dim;
+					if (dim < ALIGN(META_SIZE) + ALIGN(1)) {
+						last->status = STATUS_ALLOC;
+						memset(last + 1, 0, last->size);
+						return last + 1;
+					} else {
+						struct block_meta *new_block = (struct block_meta *)((char *)(last + 1) + block_size);
+						new_block->status = STATUS_FREE;
+						new_block->size = last->size - block_size - META_SIZE;
+						new_block->prev = last;
+						new_block->next = last->next;
+						last->size = block_size;
+						last->next = new_block;
+						last->status = STATUS_ALLOC;
+						memset(last + 1, 0, last->size);
+						return last + 1;
+					}
+				}
+			}
+		}
+		if (block_size <= last->size) {
+			size_t remaining_size = last->size - block_size;
+			if (remaining_size < ALIGN(META_SIZE) + ALIGN(1)) {
+				last->status = STATUS_ALLOC;
+				block = last;
+				memset(block + 1, 0, block->size);
+				return (last + 1);
+			}
+			struct block_meta *new_block = (struct block_meta *)((char *)(last + 1) + block_size);
+			new_block->status = STATUS_FREE;
+			new_block->size = last->size - block_size - ALIGN(META_SIZE) - ALIGN(1);
+			new_block->prev = last;
+			new_block->next = last->next;
+			if (last->next) {
+				last->next->prev = new_block;
+			}
+			last->size = block_size;
+			last->next = new_block;
+			last->status = STATUS_ALLOC;
+			block = last;
+			memset(block + 1, 0, block->size);
+			return (block + 1);
+		}
+	}
+	if (block_size >= page_size) {
+			if (!mapped_blocks) {
+				block = request_space(NULL, block_size);
+				if (!block)
+					return NULL;
+				mapped_blocks = block;
+			} else {
+				struct block_meta *last = mapped_blocks;
+				last = find_free_block(&last, block_size);
+				block = request_space(last, block_size);
+			}
+		}
+	memset(block + 1, 0, block->size);
+	return (block + 1);
 }
 
 void *os_realloc(void *ptr, size_t size)
